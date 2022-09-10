c190ad8e028ab767d2ed09942b8796deeff347b6
Hardart <kaino@bk.ru>
2022-09-10 16:51:03 +0300

    fix errors

--- server/service/bet-service.js
+++ server/service/bet-service.js
@@ -25,7 +25,7 @@ class BetService {
 
    async getAllMyBets(email) {
       const player = await this.#getPlayerInfo(email)
-      if (player.bets.length == 0) return { bets: [], points: 0 }
+      if (player.bets) return { bets: [], points: 0 }
       const finishedEvents = await eventsService.finished()
       this.#updateBetStatus(player.bets, player.data, finishedEvents)
 
